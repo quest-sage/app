ee48514d8f29a15dedf44bd484976aff695cdf13
thirdsgames <thirdsgames2018@gmail.com>
2020-12-06 13:18:49 +0000

    Fix parcel interfacing with wasm-pack

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -42,7 +42,7 @@ pub fn run() -> Result<(), JsValue> {
 
     // Manufacture the element we're gonna append
     let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust, WebAssembly, and Parcel 3!");
+    val.set_inner_html("Hello from Rust, WebAssembly, and Parcel!");
 
     body.append_child(&val)?;
 
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,3 @@
-import * as wasm from "../crate/pkg/rust_parcel";
+import * as wasm from "../crate/pkg/rust_parcel_bg.wasm";
 
 wasm.run();
