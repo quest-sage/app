fd86b3bc7e4f04a7e9fd97e9d285006ab99784d3
thirdsgames <thirdsgames2018@gmail.com>
2020-12-08 14:17:15 +0000

    Separate grid background and grid cell rendering

--- ts/tabletop.ts
+++ ts/tabletop.ts
@@ -4,27 +4,36 @@ import { ResizeSensor } from "css-element-queries";
 export class TabletopRenderer {
     coverCanvas: HTMLCanvasElement;
     gridCanvas: fabric.Canvas;
-    grid?: fabric.Line[];
+    grid?: fabric.Rect[];
+    gridDiv: HTMLDivElement;
     draggingCanvas: boolean;
     position: [number, number];
+    infiniteScrolling: boolean;
 
     constructor(tabletopContainer: HTMLDivElement) {
         this.draggingCanvas = false;
         this.position = [0, 0];
+        this.infiniteScrolling = false;
 
         const coverCanvas = document.createElement("canvas");
         coverCanvas.id = "tabletop-cover";
         coverCanvas.className = "canvas-layer";
-        coverCanvas.style.zIndex = "100";
+        coverCanvas.style.zIndex = "200";
         tabletopContainer.appendChild(coverCanvas);
         this.coverCanvas = coverCanvas;
 
         coverCanvas.addEventListener('mousedown', _ => {
             this.draggingCanvas = true;
+            if (this.infiniteScrolling) {
+                coverCanvas.requestPointerLock();
+            }
         });
 
         coverCanvas.addEventListener('mouseup', _ => {
             this.draggingCanvas = false;
+            if (this.infiniteScrolling) {
+                document.exitPointerLock();
+            }
         });
 
         coverCanvas.addEventListener('mousemove', e => {
@@ -33,16 +42,23 @@ export class TabletopRenderer {
             }
         });
 
+        const gridDiv = document.createElement("div");
+        gridDiv.id = "tabletop-grid";
+        gridDiv.className = "canvas-layer bg_grid_square64";
+        gridDiv.style.zIndex = "0";
+        gridDiv.style.opacity = '30%';
+        tabletopContainer.appendChild(gridDiv);
+        this.gridDiv = gridDiv;
+
         const gridCanvas = document.createElement("canvas");
         gridCanvas.id = "tabletop-grid";
-        gridCanvas.style.zIndex = "0";
+        gridCanvas.style.zIndex = "100";
         const gridCanvasContainer = document.createElement("div");
         gridCanvasContainer.className = "canvas-layer";
         tabletopContainer.appendChild(gridCanvasContainer);
         gridCanvasContainer.appendChild(gridCanvas);
         this.gridCanvas = new fabric.Canvas(gridCanvas);
-        gridCanvas.className = "bg_grid_square64";
-        gridCanvas.style.opacity = '30%';
+        this.drawGridCells();
 
         new ResizeSensor(tabletopContainer, this.onCanvasResize.bind(this));
     }
@@ -52,53 +68,52 @@ export class TabletopRenderer {
         this.coverCanvas.height = size.height;
 
         this.gridCanvas.setDimensions(size);
-        //this.drawGridCells();
+        
+        this.onCanvasTranslate(0, 0);
     }
 
     onCanvasTranslate(movementX: number, movementY: number): void {
         this.position[0] -= movementX;
         this.position[1] -= movementY;
-        this.gridCanvas.getContext().canvas.style.backgroundPositionX = -this.position[0] + 'px';
-        this.gridCanvas.getContext().canvas.style.backgroundPositionY = -this.position[1] + 'px';
-        //this.gridCanvas.setViewportTransform([1, 0, 0, 1, -this.position[0], -this.position[1]]);
-        //this.drawGridCells();
+
+        // Normally, we store position relative to the centre of the canvas, so that
+        // resizing etc works ergonomically.
+        // However, these x, y coords are relative to the top left of the canvas,
+        // because CSS requires us to do things in terms of the top left.
+        const x = -this.position[0] + this.gridCanvas.width! / 2;
+        const y = -this.position[1] + this.gridCanvas.height! / 2;
+
+        this.gridDiv.style.backgroundPositionX = x + 'px';
+        this.gridDiv.style.backgroundPositionY = y + 'px';
+        
+        this.gridCanvas.setViewportTransform([1, 0, 0, 1, x, y]);
     }
 
-    drawGridCells(): fabric.Line[] {
+    drawGridCells(): fabric.Rect[] {
         const CELL_SIZE = 64; // px
-        const GRID_COLOUR = "#CCCCCC";
-
-        const height = this.gridCanvas.height!;
-        const width = this.gridCanvas.width!;
-
-        const minX = this.position[0];
-        const maxX = this.position[0] + width;
-        const minY = this.position[1];
-        const maxY = this.position[1] + height;
-
-        const lineOpts = {
-            stroke: GRID_COLOUR,
-            strokeWidth: 2,
-            selectable: false,
-            evented: false,
-        };
+        const GRID_COLOUR = "#FF4343";
 
         if (this.grid !== undefined) {
             this.gridCanvas.remove(...this.grid);
         }
 
-        let lines = [];
-        for (let i = Math.floor(minX / CELL_SIZE); i <= Math.ceil(maxX / CELL_SIZE); i++) {
-            const line = new fabric.Line([i * CELL_SIZE + 1, minY, i * CELL_SIZE + 1, maxY], lineOpts);
-            lines.push(line);
-        }
-        for (let j = Math.floor(minY / CELL_SIZE); j <= Math.ceil(maxY / CELL_SIZE); j++) {
-            const line = new fabric.Line([minX, j * CELL_SIZE + 1, maxX, j * CELL_SIZE + 1], lineOpts);
-            lines.push(line);
-        }
-        this.gridCanvas.add(...lines);
-        this.grid = lines;
+        let cells = [];
+        cells.push(new fabric.Rect({
+            top: -2,
+            left: -2,
+            width: CELL_SIZE,
+            height: CELL_SIZE,
+            stroke: GRID_COLOUR,
+            fill: undefined,
+            strokeWidth: 4,
+            selectable: false,
+            evented: false,
+            rx: 2,
+            ry: 2,
+        }));
+        this.gridCanvas.add(...cells);
+        this.grid = cells;
 
-        return lines;
+        return cells;
     }
 }
