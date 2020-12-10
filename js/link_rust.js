83f5694371b98174c127a6123f406dfe4cfbece1
thirdsgames <thirdsgames2018@gmail.com>
2020-12-10 11:36:45 +0000

    Improve modularity in tabletop

--- js/link_rust.js
+++
@@ -1,2 +0,0 @@
-// This line is required to make the compiled WASM link properly.
-import * as _wasm from "../crate/Cargo.toml";
--- ts/tabletop.ts
+++ ts/tabletop.ts
@@ -1,11 +1,91 @@
 import { fabric } from "fabric";
 import { ResizeSensor } from "css-element-queries";
 
+class GridLayer {
+    /**
+     * The background div that should scroll with the tabletop, whose background image
+     * is set to a repeating image of the current grid type (e.g. squares).
+     */
+    gridBackground: HTMLDivElement;
+    /**
+     * The fabric canvas used to annotate the grid background.
+     */
+    canvas: fabric.Canvas;
+
+    annotations: fabric.Rect[];
+
+    /**
+     * @param tabletopContainer The div inside which the canvas layers should be placed.
+     */
+    constructor(tabletopContainer: HTMLDivElement) {
+        const gridBackground = document.createElement("div");
+        gridBackground.id = "tabletop-grid";
+        gridBackground.className = "canvas-layer bg_grid_square64";
+        gridBackground.style.zIndex = "0";
+        gridBackground.style.opacity = '30%';
+        tabletopContainer.appendChild(gridBackground);
+        this.gridBackground = gridBackground;
+
+        const canvas = document.createElement("canvas");
+        canvas.id = "tabletop-grid";
+        canvas.style.zIndex = "100";
+        const gridCanvasContainer = document.createElement("div");
+        gridCanvasContainer.className = "canvas-layer";
+        tabletopContainer.appendChild(gridCanvasContainer);
+        gridCanvasContainer.appendChild(canvas);
+        this.canvas = new fabric.Canvas(canvas);
+
+        this.annotations = [];
+
+        this.addDebugAnnotations();
+    }
+
+    setDimensions(size: { width: number; height: number; }) {
+        this.canvas.setDimensions(size);
+    }
+
+    setViewportTransform(matrix: number[]) {
+        // The 4th and 5th entries of this matrix are the X and Y translations.
+        this.gridBackground.style.backgroundPositionX = matrix[4] + 'px';
+        this.gridBackground.style.backgroundPositionY = matrix[5] + 'px';
+
+        this.canvas.setViewportTransform(matrix);
+    }
+
+    addDebugAnnotations() {
+        const CELL_SIZE = 64; // px
+        const GRID_COLOUR = "#FF4343";
+
+        this.canvas.remove(...this.annotations);
+
+        let cells = [];
+        cells.push(new fabric.Rect({
+            top: -2,
+            left: -2,
+            width: CELL_SIZE,
+            height: CELL_SIZE,
+            stroke: GRID_COLOUR,
+            fill: undefined,
+            strokeWidth: 4,
+            selectable: false,
+            evented: false,
+            rx: 2,
+            ry: 2,
+        }));
+        this.canvas.add(...cells);
+        this.annotations = cells;
+    }
+}
+
 export class TabletopRenderer {
+    /**
+     * Used for top-level annotations on the tabletop, and to capture mouse events from propagating to lower layers if required.
+     */
     coverCanvas: HTMLCanvasElement;
-    gridCanvas: fabric.Canvas;
-    grid?: fabric.Rect[];
-    gridDiv: HTMLDivElement;
+    /**
+     * The canvas layer used for drawing the grid, and annotations on the grid.
+     */
+    gridLayer: GridLayer;
     draggingCanvas: boolean;
     position: [number, number];
     infiniteScrolling: boolean;
@@ -42,23 +122,7 @@ export class TabletopRenderer {
             }
         });
 
-        const gridDiv = document.createElement("div");
-        gridDiv.id = "tabletop-grid";
-        gridDiv.className = "canvas-layer bg_grid_square64";
-        gridDiv.style.zIndex = "0";
-        gridDiv.style.opacity = '30%';
-        tabletopContainer.appendChild(gridDiv);
-        this.gridDiv = gridDiv;
-
-        const gridCanvas = document.createElement("canvas");
-        gridCanvas.id = "tabletop-grid";
-        gridCanvas.style.zIndex = "100";
-        const gridCanvasContainer = document.createElement("div");
-        gridCanvasContainer.className = "canvas-layer";
-        tabletopContainer.appendChild(gridCanvasContainer);
-        gridCanvasContainer.appendChild(gridCanvas);
-        this.gridCanvas = new fabric.Canvas(gridCanvas);
-        this.drawGridCells();
+        this.gridLayer = new GridLayer(tabletopContainer);
 
         new ResizeSensor(tabletopContainer, this.onCanvasResize.bind(this));
     }
@@ -67,7 +131,7 @@ export class TabletopRenderer {
         this.coverCanvas.width = size.width;
         this.coverCanvas.height = size.height;
 
-        this.gridCanvas.setDimensions(size);
+        this.gridLayer.setDimensions(size);
         
         this.onCanvasTranslate(0, 0);
     }
@@ -80,40 +144,9 @@ export class TabletopRenderer {
         // resizing etc works ergonomically.
         // However, these x, y coords are relative to the top left of the canvas,
         // because CSS requires us to do things in terms of the top left.
-        const x = -this.position[0] + this.gridCanvas.width! / 2;
-        const y = -this.position[1] + this.gridCanvas.height! / 2;
-
-        this.gridDiv.style.backgroundPositionX = x + 'px';
-        this.gridDiv.style.backgroundPositionY = y + 'px';
+        const x = -this.position[0] + this.coverCanvas.width! / 2;
+        const y = -this.position[1] + this.coverCanvas.height! / 2;
         
-        this.gridCanvas.setViewportTransform([1, 0, 0, 1, x, y]);
-    }
-
-    drawGridCells(): fabric.Rect[] {
-        const CELL_SIZE = 64; // px
-        const GRID_COLOUR = "#FF4343";
-
-        if (this.grid !== undefined) {
-            this.gridCanvas.remove(...this.grid);
-        }
-
-        let cells = [];
-        cells.push(new fabric.Rect({
-            top: -2,
-            left: -2,
-            width: CELL_SIZE,
-            height: CELL_SIZE,
-            stroke: GRID_COLOUR,
-            fill: undefined,
-            strokeWidth: 4,
-            selectable: false,
-            evented: false,
-            rx: 2,
-            ry: 2,
-        }));
-        this.gridCanvas.add(...cells);
-        this.grid = cells;
-
-        return cells;
+        this.gridLayer.setViewportTransform([1, 0, 0, 1, x, y]);
     }
 }
